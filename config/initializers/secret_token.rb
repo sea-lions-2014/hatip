31088c90f073ddd629689dceb646886347be9a60
Apprentice <apprentice@DBC-Infinity-2.local>
2014-02-27 12:05:54 -0800

    created rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hatip::Application.config.secret_token = '056b2b2e559e360d7611742e7e6707df61edcfe582c9e35f2e8bb215ccb2e88f7d928da37f8ec9b63224f55bb0236db416f80ed05588d551b91196241d318d3a'
