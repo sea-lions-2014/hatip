942bf42faf0185d8977355bf9164cca75ad16408
& <pair+emfoley.mechamanny@devbootcamp.com>
2014-02-28 14:42:35 -0800

    prepare for testing, clean up routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find(params[:id])
-    @post = Post.new
+
+     @user = User.find(params[:id])
+     @post = Post.new
+
   end
 
   def edit
