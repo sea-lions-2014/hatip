b559e6692bf665d77978f4e05570da4c44b2e78b
Alexander Leishman <leishman3@gmail.com>
2014-03-01 12:03:38 -0800

    more tests for production fixing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,8 +13,8 @@ Hatip::Application.configure do
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
-  config.assets.js_compressor = NoCompression.new
-  config.assets.css_compressor = NoCompression.new
+  # config.assets.js_compressor = NoCompression.new
+  # config.assets.css_compressor = NoCompression.new
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
