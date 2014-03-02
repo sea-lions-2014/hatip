cdab7e1313f0a27c1fe4e425a24deec1659b5c57
Alexander Leishman <leishman3@gmail.com>
2014-03-01 18:43:05 -0800

    coinbase callback fix

--- app/controllers/tips_controller.rb
+++ app/controllers/tips_controller.rb
@@ -21,6 +21,7 @@ class TipsController < ApplicationController
       status: 					order['status'],
       receive_address: 	order['receive_address']
     }
+    puts tip_info
 
     user.tips.create(tip_info)
     render json: { success: "success" }
@@ -32,37 +33,37 @@ end
 # Coinbase callback data looks like this:
 
 # {"order"=>
-# 	{	"id"=>"ATNON22I", 
-# 		"created_at"=>"2014-03-01T16:46:37-08:00",
-# 		"status"=>"completed",
-# 		"total_btc"=>
-# 			{
-# 		  	"cents"=>43650,
-# 		   	"currency_iso"=>"BTC"
-# 			}, 
-# 		"total_native"=>
-#
-# 			{
-# 				"cents"=>25, 
-# 				"currency_iso"=>"USD"
-# 			}, 
-# 		"custom"=>"#user{ self.user.id }-post{ self.id }",
-# 		"receive_address"=>"1JVPjWF9igFFzSdYCSegpWZX2XdJv3p945",
-# 		"button"=>
-# 			{
-# 				"type"=>"donation",
-# 				"name"=>"Tip for Alicia Hartmann",
-# 				"description"=>"Tip",
-# 				"id"=>"f6b64e664a9d6c05d5d236f301d560dc"
-#				},
-# 		"transaction"=>
-#				{
-#					"id"=>"5312800a4ed082d08a0045ea",
-# 				"hash"=>"6c7e54414d613d5a0323bfa0caa93b232ce2a1fea1bbcc94630e860db5c805c4",
-# 				"confirmations"=>0
-# 			}
-# 	}, 
-# 	"action"=>"create", 
-# 	"controller"=>"tips", 
-# 	"tip"=>{}
+	# {	"id"=>"ATNON22I", 
+	# 	"created_at"=>"2014-03-01T16:46:37-08:00",
+	# 	"status"=>"completed",
+	# 	"total_btc"=>
+	# 		{
+	# 	  	"cents"=>43650,
+	# 	   	"currency_iso"=>"BTC"
+	# 		}, 
+	# 	"total_native"=>
+
+	# 		{
+	# 			"cents"=>25, 
+	# 			"currency_iso"=>"USD"
+	# 		}, 
+	# 	"custom"=>"#user{ self.user.id }-post{ self.id }",
+	# 	"receive_address"=>"1JVPjWF9igFFzSdYCSegpWZX2XdJv3p945",
+	# 	"button"=>
+	# 		{
+	# 			"type"=>"donation",
+	# 			"name"=>"Tip for Alicia Hartmann",
+	# 			"description"=>"Tip",
+	# 			"id"=>"f6b64e664a9d6c05d5d236f301d560dc"
+	# 			},
+	# 	"transaction"=>
+	# 			{
+	# 				"id"=>"5312800a4ed082d08a0045ea",
+	# 			"hash"=>"6c7e54414d613d5a0323bfa0caa93b232ce2a1fea1bbcc94630e860db5c805c4",
+	# 			"confirmations"=>0
+	# 		}
+	# }, 
+	# "action"=>"create", 
+	# "controller"=>"tips", 
+	# "tip"=>{}
 # }
