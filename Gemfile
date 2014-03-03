d79f38624a7582d483f29c82d5c96282a98c77a7
Alexander Leishman <null>
2014-03-02 23:48:44 -0800

    first steps at post form modal functionality

--- Gemfile
+++ Gemfile
@@ -22,6 +22,7 @@ end
 group :test, :development do
   gem 'rspec-rails'
   gem 'dotenv-rails'
+  gem 'better_errors'
 end
 
 group :test do
--- app/assets/javascripts/profile.js
+++ app/assets/javascripts/profile.js
@@ -4,16 +4,17 @@ $(function(){
 
 var Profile = {
   bindEventListeners: function(){
-    $('#new_post_button').click(Profile.showForm);
+    $('#new_post_button').click(Profile.displayModal);
     $("#new_post").on("ajax:success", Profile.appendPost);
   },
 
-  showForm: function(){
-    $('#new_post').show();
+  displayModal: function(){
+    // Profile.updateModalElements();
+    $('#postModal').show();
   },
 
   appendPost: function(e, data, status, xhr){
     $('.posts-container').append(data);
-  }
+  },
 }
 
