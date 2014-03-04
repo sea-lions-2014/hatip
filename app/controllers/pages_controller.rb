e67748f619e6ac8145e079e232458b60708d3824
Emmanuel Kaunitz <ekaunitz@gmail.com>
2014-03-03 22:06:02 -0800

    added infinite scroll for home page.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
   before_filter :redirect_if_empty_profile
 
   def index
-    @posts = Post.find(:all, :limit => 20)
+	@posts = Post.paginate(:page => params[:page], :per_page => 8)
   end
 end
