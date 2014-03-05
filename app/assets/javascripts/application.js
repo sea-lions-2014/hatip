d9e48e38536628ab64db3a1fe9c722bc6babff08
Emmanuel Kaunitz <ekaunitz@gmail.com>
2014-03-05 14:35:34 -0800

    jasmine test running

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require 
 //= require jquery-ui-1.10.4.custom.min
 //= require stripe_checkout
 //= require bootstrap-modal.js
---
+++ spec/javascripts/jasmine_examples/ProfileCardsSpec.js
@@ -0,0 +1,6 @@
+describe("ProfileCards", function(){
+
+	it("should respond to click events", function() {
+		expect(1).toEqual(1)
+	})
+})
